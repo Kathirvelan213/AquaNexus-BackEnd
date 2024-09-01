5fa9523baa19ac6322fd82e01ec6ce01c3c63804
kathirvelan <kathirvelanj1@gmail.com>
2024-09-01 11:07:33 +0530

    Installed express

---
+++ server.js
@@ -0,0 +1,26 @@
+// import { GetAllFishCatchData2 } from '../BAL/FishPopulationManager.js';
+
+const express = require('express');
+const app = express();
+//const port = 3000;
+const port=process.env.PORT;
+
+
+app.use(express.static('public'));
+app.get('/api/message', (req, res) => {
+    res.send('helloo');
+    // (async ()=>{const jsonfile2=await GetAllFishCatchData2();
+    //     console.log(jsonfile2);
+    // res.json({ jsonfile2 });
+    // })();
+    
+    
+});
+
+app.listen(port||3000, () => {
+    console.log(`Server is listening at https://testwebappserver.azurewebsites.net`);
+});
+
+
+
+
