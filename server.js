89fa25b588b0683ce47a6555ec86224a8b46af2b
kathirvelan <kathirvelanj1@gmail.com>
2024-09-02 20:20:42 +0530

    installed cors

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const populationManager = require('./BAL/FishPopulationManager');
 
 const express = require('express');
 const app = express();
+const cors=require('cors')
 const port=process.env.PORT;
+
+const corsOptions = {
+    origin: 'http://localhost:5173/',
+  }
 
 
 app.use(express.static('public'));
-app.get('/api/Population/All', (req, res) => {
+app.get('/api/Population/All',cors(), (req, res) => {
     (async ()=>{const jsonfile=await populationManager.GetAllFishCatchData();
         // console.log(jsonfile2);
     res.json({ jsonfile });
